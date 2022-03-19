620371e82d455b1cf71cd170d6b1df35d76e9371
Ebubekir-Tas <tas_ebubekir@yahoo.com>
2022-03-19 17:42:32 -0400

    cors

--- ReactQuery-Express/Backend/server.js
+++ ReactQuery-Express/Backend/server.js
@@ -4,12 +4,6 @@ const cors = require('cors');
 
 app.use(cors());
 
-app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
- });
-
 app.get('/userMsg', (req, res)=> {
   res.json({message: 'hello'})
 })
