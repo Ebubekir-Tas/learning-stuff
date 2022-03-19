ed95d982c98a2e59f8e9d540facd800632c74c72
Ebubekir-Tas <tas_ebubekir@yahoo.com>
2022-03-19 18:30:01 -0400

    switched from fetch to axios

--- ReactQuery-Express/Frontend/src/Hooks/useMsg.js
+++ ReactQuery-Express/Frontend/src/Hooks/useMsg.js
@@ -1,8 +1,9 @@
 import { useQuery } from "react-query";
+import axios from 'axios';
 
 const getUserMsg = async () => {
-  const res = await fetch('http://localhost:3001/userMsg')
-  return res.json();
+  const res = await axios.get('http://localhost:3001/userMsg')
+  return res.data;
 }
 
 export default function useMsg() {
